ea34a072dbf7af65a0539c1b6acdbc42de84788d
Nick Baker <ndbaker1@outlook.com>
2022-03-11 20:39:20 -0600

    add README for backend folder and reformat server.py

---
+++ backend/README.md
@@ -0,0 +1,15 @@
+# Backend
+
+Components
+- Flask
+- Google Drive API v3
+
+## Install
+```bash
+python -m pip install -r requirements.txt
+```
+
+## Run
+```bash
+python server.py
+```
--- backend/server.py
+++ backend/server.py
@@ -12,41 +12,44 @@ import drive
 from definitions import *
 
 
+app = Flask(__name__)
+
+# service = drive.init_drive_service()
+
+
+@app.post('/' + Endpoints.Create)
+def create_file():
+    if request.headers.get('Content-Type') == 'application/json':
+        file_name = request.json[RequestBodyFields.Filename]
+        password = request.json[RequestBodyFields.Password]
+        # make sure that no file already exists with this name.
+        file_metadata = {'name': file_name}
+        # upload = service.files().create(
+        #     body=file_metadata,
+        #     media_body="",
+        #     fields='id',
+        # ).execute()
+        return "good!"
+
+
+@app.post('/' + Endpoints.Edit)
+def download_file():
+    if request.headers.get('Content-Type') == 'application/json':
+        file_name = request.json[RequestBodyFields.Filename]
+        password = request.json[RequestBodyFields.Password]
+        # response = service.files().list(
+        #     q=f"name='{file_name}'",
+        #     spaces='drive',
+        #     fields='nextPageToken, files(id, name)',
+        # ).execute()
+        # files = response.get('files', [])
+        # # test for the existence of the file
+        # if not files:
+        #     return {"error": f"no files exists with the name {file_name}"}
+
+        # requested_file = files[0]
+        return "good!"
+
+
 if __name__ == '__main__':
-    # service = drive.init_drive_service()
-
-    app = Flask(__name__)
-
-    @app.post('/' + Endpoints.Create)
-    def create_file():
-        if request.headers.get('Content-Type') == 'application/json':
-            file_name = request.json[RequestBodyFields.Filename]
-            password = request.json[RequestBodyFields.Password]
-            # make sure that no file already exists with this name.
-            file_metadata = {'name': file_name}
-            # upload = service.files().create(
-            #     body=file_metadata,
-            #     media_body="",
-            #     fields='id',
-            # ).execute()
-            return "good!"
-
-    @app.post('/' + Endpoints.Edit)
-    def download_file():
-        if request.headers.get('Content-Type') == 'application/json':
-            file_name = request.json[RequestBodyFields.Filename]
-            password = request.json[RequestBodyFields.Password]
-            # response = service.files().list(
-            #     q=f"name='{file_name}'",
-            #     spaces='drive',
-            #     fields='nextPageToken, files(id, name)',
-            # ).execute()
-            # files = response.get('files', [])
-            # # test for the existence of the file
-            # if not files:
-            #     return {"error": f"no files exists with the name {file_name}"}
-
-            # requested_file = files[0]
-            return "good!"
-
     app.run(debug=True)
