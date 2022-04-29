c36c99d846418ab40d55c0b0dd466c01a7bb299f
Nick Baker <ndbaker1@outlook.com>
2022-04-28 19:17:25 -0500

    updated request fields

--- backend/definitions.py
+++ backend/definitions.py
@@ -1,28 +1,33 @@
+from enum import Enum
+
 
 """
 Design definitions/aspects of the backend service.
 """
 
 
-class RequestBodyFields:
-    Filename = 'file_name'
-    Password = 'password'
+class PasswordMode(Enum):
+    Owner = "owner"
+    Shared = "shared"
 
 
-class Endpoints:
-    """
-    User does not have a file open
-    """
+class RequestBodyField(Enum):
+    Filename = "file_name"
+    Password = "password"
+    SharedSecrets = "shared_secrets"
+    NewPassword = "new_password"
+    Content = "content"
+
+
+class Endpoint:
     # Create a brand new file as the owner
-    Create = 'create'
+    Create = "create"
     # Edit a file gaining access using a password
-    Edit = 'edit-owner'
+    Edit = "edit-owner"
     # Edit a file gaining access using shared secrets
-    EditShared = 'edit-shared'
-    """
-    User has a file open
-    """
+    EditShared = "edit-shared"
+
     # generate shared secrets for the currently open document
-    SharedSecrets = 'shared-secrets'
+    SharedSecrets = "shared-secrets"
     # change the password for the currently open document
-    ChangePassword = 'change-password'
+    ChangePassword = "change-password"
